aef141ac9edb085840cce33882abb258f0ff8055
ahashmi5401 <ahashmi5401@gmail.com>
2026-03-29 05:49:37 +0500

    Join Two Array By Id

---
+++ LeetCode/medium/Join-Two-Array-by-Id.js
@@ -0,0 +1,44 @@
+// Given two arrays arr1 and arr2, return a new array joinedArray. All the objects in each of the two inputs arrays will contain an id field that has an integer value. 
+
+// joinedArray is an array formed by merging arr1 and arr2 based on their id key. The length of joinedArray should be the length of unique values of id. The returned array should be sorted in ascending order based on the id key.
+
+// If a given id exists in one array but not the other, the single object with that id should be included in the result array without modification.
+
+// If two objects share an id, their properties should be merged into a single object:
+
+// If a key only exists in one object, that single key-value pair should be included in the object.
+// If a key is included in both objects, the value in the object from arr2 should override the value from arr1.
+
+
+
+
+//no duplicate id soeasuly , merge
+var joinArr = (arr1 , arr2 , fn) => {
+    let newArr = [...arr1 , ...arr2];
+    for (let i = 0; i < newArr.length; i++) {
+        for (let j = 0; j < newArr.length - 1; j++) {
+            if(fn(newArr[j]) > fn(newArr[j+1])){
+                console.log('mai chl bhi raha hu kya');
+                let temp = newArr[j];
+                newArr[j] = newArr[j+1];
+                newArr[j+1] = temp;
+            }
+        }
+    }
+    return newArr
+}
+
+const getObjValue = (d) =>{
+    return d.id
+} 
+
+arr1 = [
+    {"id": 1, "x": 1},
+    {"id": 2, "x": 9}
+], 
+arr2 = [
+    {"id": 3, "x": 5}
+]
+
+console.log(joinArr(arr1 , arr2 , getObjValue));
+
