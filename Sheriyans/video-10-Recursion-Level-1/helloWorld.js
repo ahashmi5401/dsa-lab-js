db5045c8dac925929c7bd5b0c09fe3c69bf956cf
ahashmi5401 <ahashmi5401@gmail.com>
2026-04-07 03:15:14 +0500

    Print n to 1 number by using Recursion

--- Sheriyans/video-10-Recursion-Level-1/helloWorld.js
+++ Sheriyans/video-10-Recursion-Level-1/helloWorld.js
@@ -1,10 +1,12 @@
 //Print n time msg by using Recursion
-
+//print number 1 to n 
 function msg(n){
     if(n === 0) return `End`
     console.log('Hello World');
+    
     msg(n - 1) //ab ye 5 - 1 karke 4 =>> call karea '
 
     console.log(n); //last in first out last me gaya 1 magr call stack se sabse pehle one nikle ga LIFO
 }
 msg(5)
+
