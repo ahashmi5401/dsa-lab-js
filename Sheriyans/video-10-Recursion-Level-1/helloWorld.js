dde3b46a94fa8bdac46b6ba11ff687e99b5900d7
ahashmi5401 <ahashmi5401@gmail.com>
2026-04-07 03:07:24 +0500

    Recursion => Level 1

---
+++ Sheriyans/video-10-Recursion-Level-1/helloWorld.js
@@ -0,0 +1,10 @@
+//Print n time msg by using Recursion
+
+function msg(n){
+    if(n === 0) return `End`
+    console.log('Hello World');
+    msg(n - 1) //ab ye 5 - 1 karke 4 =>> call karea '
+
+    console.log(n); //last in first out last me gaya 1 magr call stack se sabse pehle one nikle ga LIFO
+}
+msg(5)
